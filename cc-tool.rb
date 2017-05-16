a3f74f81b540799a01b6a3b7d0020a88f717a760
Emmanuel Blot <emmanuel.blot@free.fr>
2017-05-16 17:28:46 +0200

    Add CLI tool for Texas Instrument CC Debugger

---
+++ cc-tool.rb
@@ -0,0 +1,19 @@
+class CcTool < Formula
+  desc "Support for Texas Instruments CC Debugger."
+  homepage "https://sourceforge.net/projects/cctool"
+  head "https://github.com/dashesy/cc-tool.git"
+
+  depends_on "boost" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libusb"
+
+  def install
+    pkgconfig = Formulary.factory 'pkg-config'
+
+    ENV['PKG_CONFIG'] = "#{pkgconfig.opt_prefix}/bin/pkg-config"
+
+    system "./configure", "--prefix=#{prefix}", "--disable-debug"
+    system "make"
+    system "make", "install"
+  end
+end
